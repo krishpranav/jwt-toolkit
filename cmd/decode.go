eeddf2440f3d710039bc6e48d4febde704cca307
krishpranav <krisna.pranav@gmail.com>
2021-07-15 19:18:15 +0530

    cmd: decode

--- cmd/decode.go
+++ cmd/decode.go
@@ -9,13 +9,10 @@ import (
 
 	"github.com/dgrijalva/jwt-go"
 	jwtInterface "github.com/hahwul/jwt-hack/pkg/jwt"
-
-	//. "github.com/logrusorgru/aurora"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
-// decodeCmd represents the decode command
 var decodeCmd = &cobra.Command{
 	Use:   "decode [JWT Token]",
 	Short: "Decode JWT to JSON",
@@ -36,7 +33,32 @@ var decodeCmd = &cobra.Command{
 			data, _ := json.Marshal(token.Claims)
 			json.Unmarshal([]byte(data), &jdata)
 
+			if jdata["iat"] != nil {
+				iatf := jdata["iat"].(float64)
+				iats := fmt.Sprintf("%.0f", iatf)
+				iat, _ := strconv.Atoi(iats)
+				iatt := time.Unix(0, int64(iat))
+				log.WithFields(logrus.Fields{
+					"IAT":  iats,
+					"TIME": iatt,
+				}).Info("Issued At Time")
+			}
+			if jdata["exp"] != nil {
+				expf := jdata["exp"].(float64)
+				exps := fmt.Sprintf("%.0f", expf)
+				exp, _ := strconv.Atoi(exps)
+				expt := time.Unix(0, int64(exp))
+				log.WithFields(logrus.Fields{
+					"EXP":  exps,
+					"TIME": expt,
+				}).Info("Expiraton Time")
+			}
+			fmt.Println(string(data))
+		} else {
+			var log = logrus.New()
+			log.Out = os.Stdout
+			log.Error("Arguments Error")
+			log.Error("e.g jwt-hack decode {JWT_CODE}")
 		}
-
 	},
 }
