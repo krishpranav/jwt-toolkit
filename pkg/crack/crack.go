1340572709770d4e9c0d2d95153fccb978ec5cfa
krishpranav <krisna.pranav@gmail.com>
2021-07-15 18:54:32 +0530

    pkg: crack functions

--- pkg/crack/crack.go
+++ pkg/crack/crack.go
@@ -20,7 +20,7 @@ func Crack(mode, token, data string, concurrency, max int, power bool, verbose b
 	if mode == "brute" {
 		bf := GenerateBruteforcePayloads(data)
 		RunTestingJWT(token, bf, concurrency, verbose)
-	} else {
+	} else { // if dict
 		var words []string
 		ff, err := readLinesOrLiteral(data)
 		_ = err
@@ -28,6 +28,7 @@ func Crack(mode, token, data string, concurrency, max int, power bool, verbose b
 			words = append(words, word)
 		}
 
+		// Remove Deplicated value
 		words = unique(words)
 		log.WithFields(logrus.Fields{
 			"size": len(words),
@@ -54,4 +55,54 @@ func RunTestingJWT(token string, lists []string, concurrency int, verbose bool) 
 		s.Suffix = str
 		s.Start()
 	}
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for word := range wordlists {
+				nowLine = nowLine + 1
+				percent := (float64(nowLine) / float64(lenWordlist) * 100)
+				str := fmt.Sprintf(" Cracking.. [%d/%d][%0.2f%%]", nowLine, lenWordlist, percent)
+				s.Suffix = str
+				if !found {
+					result, token := jwtInterface.JWTdecodeWithVerify(token, word)
+					_ = token
+					if result {
+						mutex.Lock()
+						s.Stop()
+						log.WithFields(logrus.Fields{
+							"Signature": "Verified",
+							"Word":      word,
+						}).Info("Found! Token signature secret is " + word)
+						found = true
+						secret = word
+						s.Start()
+						mutex.Unlock()
+					} else {
+						if verbose {
+							log.WithFields(logrus.Fields{
+								"word": word,
+							}).Info("Invalid signature")
+						}
+					}
+				}
+			}
+		}()
+	}
+
+	for k, v := range lists {
+		_ = k
+		wordlists <- v
+	}
+
+	close(wordlists)
+	wg.Wait()
+
+	if !verbose {
+		s.Stop()
+	}
+	if found {
+		fmt.Println("[+] Found! JWT signature secret:", color.BrightYellow(secret))
+	}
+	fmt.Println("[+] Finish crack mode")
 }
