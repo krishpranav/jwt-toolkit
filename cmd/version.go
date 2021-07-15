385d3784b1863fa592ee48d73d4a7c1d857d7281
krishpranav <krisna.pranav@gmail.com>
2021-07-15 19:24:36 +0530

    cmd: version

--- cmd/version.go
+++ cmd/version.go
@@ -7,11 +7,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show version",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(printing.VERSION)
 	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+
 }
